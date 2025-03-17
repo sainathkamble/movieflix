6b7517a3ef703ef372fdc4e39abece93162806ae
Sainath Kamble <kamblesainath41@gmail.com>
2025-03-17 20:06:25 +0530

    Update searchbar.jsx

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -49,7 +49,6 @@ export const SearchBar = () => {
   return (
     <div className="h-full w-full bg-gray-950">
       <div className="h-auto w-full p-4 bg-gray-950 grid grid-cols-1 grid-rows-2 place-items-center">
-        <p className="h-auto w-auto text-white text-2xl">UniAcco Frontend Intenship Assignment</p>
         <input type="text" placeholder="Search"
           onChange={handleSearch} value={searchQuery}
           className="h-[8vh] w-1/2 p-4 rounded-full bg-slate-800 text-white" 
